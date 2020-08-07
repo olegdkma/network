1d3ac3d2ba06f96a28401f5815b19afb649200d5
KU <olegdkma@gmail.com>
2020-08-07 17:30:04 +0300

    Updates

---
+++ precache-manifest.2fabc49684b87607c83ac56c39431711.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47d4cc695b24463e3b445e0a72a21ffa",
+    "url": "/network/index.html"
+  },
+  {
+    "revision": "6d92b3a3486b7e03baf1",
+    "url": "/network/static/css/main.631c89cc.chunk.css"
+  },
+  {
+    "revision": "0e86199785005fdc9715",
+    "url": "/network/static/js/2.652c686f.chunk.js"
+  },
+  {
+    "revision": "69ff2f716ce9f4272c22339c3f8b9747",
+    "url": "/network/static/js/2.652c686f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d92b3a3486b7e03baf1",
+    "url": "/network/static/js/main.a9f4524b.chunk.js"
+  },
+  {
+    "revision": "869eb8c568d9792fa8d3",
+    "url": "/network/static/js/runtime-main.27354c95.js"
+  }
+]);
